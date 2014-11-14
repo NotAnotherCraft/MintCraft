1e166aa00aeedef7d7b253737addf0a7facb092d
Aaron Haun <aaron@haun.guru>
2014-11-14 18:09:20 -0500

    Added Empty Preinit and onInit

--- src/main/java/net/notanothercraft/mintcraft/MintCraftMod.java
+++ src/main/java/net/notanothercraft/mintcraft/MintCraftMod.java
@@ -1,8 +1,20 @@
 package net.notanothercraft.mintcraft;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "MintCraft")
 public class MintCraftMod {
+
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent e){
+
+    }
+
+    @Mod.EventHandler
+    public void onInit(FMLInitializationEvent e){
+
+    }
 
 }
