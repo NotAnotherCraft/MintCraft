f8e9a2bc4bbafb6a14ab59ee89aaf306ddb8237f
Aaron Haun <aaron@haun.guru>
2014-11-14 19:45:25 -0500

    fix iron coin

--- src/main/java/net/notanothercraft/mintcraft/item/ItemCoinIron.java
+++ src/main/java/net/notanothercraft/mintcraft/item/ItemCoinIron.java
@@ -2,12 +2,13 @@ package net.notanothercraft.mintcraft.item;
 
 /**
  * Created by Azreth on 11/14/14.
+ * An iron coin
  */
 public class ItemCoinIron extends ItemCoin{
     public ItemCoinIron(){
         super();
         this.setWorth(5);
-        this.setUnlocalizedName("iron coin");
+        this.setUnlocalizedName("ironcoin");
         this.setTextureName("mintCraft:coiniron");
     }
 }
