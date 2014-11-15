2fe8b41c7067bde8e08e77ebabdd02a403f58f01
Aaron Haun <aaron@haun.guru>
2014-11-14 22:34:05 -0500

    Fix Coinbag Inheritance

--- src/main/java/net/notanothercraft/mintcraft/item/ItemCoinBag.java
+++ src/main/java/net/notanothercraft/mintcraft/item/ItemCoinBag.java
@@ -1,13 +1,14 @@
 package net.notanothercraft.mintcraft.item;
 
+import net.minecraft.item.Item;
+
 /**
  * Created by Azreth on 11/14/14.
  * A bag for your coin
  */
-public class ItemCoinBag extends ItemCoin{
+public class ItemCoinBag extends Item {
     public ItemCoinBag(){
         super();
-        this.setWorth(0);
         this.setUnlocalizedName("coinbag");
         this.setTextureName("mintcraft:coinbag");
     }
