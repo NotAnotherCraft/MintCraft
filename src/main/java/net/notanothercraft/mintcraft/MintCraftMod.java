502b88424e39ed5ea6c66a2cfc5992729aee5c30
Azreth <azreth@notanothercraft.net>
2014-11-14 20:47:53 -0500

    Added Coinbag -azreth

--- src/main/java/net/notanothercraft/mintcraft/MintCraftMod.java
+++ src/main/java/net/notanothercraft/mintcraft/MintCraftMod.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.creativetab.CreativeTabs;
 import net.notanothercraft.mintcraft.item.ItemCoin;
+import net.notanothercraft.mintcraft.item.ItemCoinBag;
 import net.notanothercraft.mintcraft.item.ItemCoinIron;
 
 @Mod(modid = "mintCraft", name = "MintCraft", version = "0.0.1")
@@ -17,16 +18,20 @@ public class MintCraftMod {
     public CreativeTabs mintCreativeTab;
     public ItemCoin itemCoin;
     public ItemCoinIron itemCoinIron;
+    public ItemCoinBag itemCoinBag;
 
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent e){
         mintCreativeTab = new MintCreativeTab();
         itemCoin = new ItemCoin();
         itemCoinIron = new ItemCoinIron();
+        itemCoinBag = new ItemCoinBag();
         itemCoin.setCreativeTab(mintCreativeTab);
         itemCoinIron.setCreativeTab(mintCreativeTab);
+        itemCoinBag.setCreativeTab(mintCreativeTab);
         GameRegistry.registerItem(itemCoin,"coin");
         GameRegistry.registerItem(itemCoinIron,"iron coin");
+        GameRegistry.registerItem(itemCoinBag,"Coin bag");
     }
 
     @Mod.EventHandler
---
+++ src/main/java/net/notanothercraft/mintcraft/item/ItemCoinBag.java
@@ -0,0 +1,15 @@
+package net.notanothercraft.mintcraft.item;
+
+/**
+ * Created by Azreth on 11/14/14.
+ * A bag for your coin
+ */
+public class ItemCoinBag extends ItemCoin{
+    public ItemCoinBag(){
+        super();
+        this.setWorth(0);
+        this.setUnlocalizedName("coinbag");
+        this.setTextureName("mintcraft:coinbag");
+    }
+
+}
